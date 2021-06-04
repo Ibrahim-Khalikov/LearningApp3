da416cde490bc5a98f3416a707659763539e5624
Ibrahim Khalikov <starlink050@gmail.com>
2021-06-04 06:27:38 +0500

    Initial Commit

---
+++ LearningApp3/LearningApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp3App.swift
+//  LearningApp3
+//
+//  Created by cloud_vfx on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
